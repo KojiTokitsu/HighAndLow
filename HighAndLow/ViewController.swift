44dd92844d4466e1c4f6bf3762e0f16fced121c5
KojiTokitsu <Koji.Tokitsu>
2016-05-01 03:09:45 +0900

    初回実装

--- HighAndLow/ViewController.swift
+++ HighAndLow/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var Label: UILabel!
+    @IBOutlet var ButtonHigh: UIButton!
+    @IBOutlet var ButtonLow: UIButton!
+    @IBOutlet var Result: UILabel!
+    @IBOutlet var Score: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Label.text = String(rand() % 13 + 1)
+        Result.text?.removeAll()
+        Score.text = String(0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didButtonTouchDown(sender: UIButton) {
+        let currentNumber = Int32(Label.text!)
+        let nextNumber = rand() % 13 + 1
+        var correct = false
+        switch(sender){
+        case ButtonHigh:
+            if currentNumber < nextNumber {
+                correct = true
+            }
+        case ButtonLow:
+            if currentNumber > nextNumber {
+                correct = true
+            }
+        default:
+            print("Unknown button is touched down.")
+        }
+        
+        if correct {
+            Result.text = "Correct!"
+            Result.textColor = UIColor.blackColor()
+            Score.text = String(Int(Score.text!)!+1)
+        }
+        else{
+            Result.text = "Game over"
+            Result.textColor = UIColor.redColor()
+            Score.text = String(0)
+        }
+        
+        Label.text = String(nextNumber)
+    }
 }
 
